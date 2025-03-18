29e3f8a4b9eb3433e0c4d80d3abb193ac63e6581
Tamim Sharif <202314083@student.mist.ac.bd>
2025-03-18 10:00:41 +0600

    home.fxml updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,26 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
+        primaryStage.setTitle("MIST SMSys");
+        primaryStage.getIcons().add(new Image("/img/icon.png"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
